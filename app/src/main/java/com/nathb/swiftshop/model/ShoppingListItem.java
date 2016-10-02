640090c6f575cf5bbb2f594f8d2c179105426460
nathb <nath04@gmail.com>
2016-10-02 11:21:39 +1100

    Fixed Shopping List Item order

--- app/src/main/java/com/nathb/swiftshop/model/ShoppingListItem.java
+++ app/src/main/java/com/nathb/swiftshop/model/ShoppingListItem.java
@@ -44,12 +44,20 @@ public class ShoppingListItem extends RealmObject implements Comparable<Shopping
             return 1;
         }
 
-        // Order by checked status
-        Boolean a = Boolean.valueOf(this.isChecked());
-        Boolean b = Boolean.valueOf(o.isChecked());
-        int isCheckedComparison = a.compareTo(b);
+        // First order by checked status
+        Boolean aChecked = Boolean.valueOf(this.isChecked());
+        Boolean bChecked = Boolean.valueOf(o.isChecked());
+        int isCheckedComparison = aChecked.compareTo(bChecked);
         if (isCheckedComparison != 0) {
             return isCheckedComparison;
+        }
+
+        // Then order by category
+        Integer aOrder = this.getItem().getCategory().getOrder();
+        Integer bOrder = o.getItem().getCategory().getOrder();
+        int orderComparison = aOrder.compareTo(bOrder);
+        if (orderComparison != 0) {
+            return orderComparison;
         }
 
         // Then order alphabetically
